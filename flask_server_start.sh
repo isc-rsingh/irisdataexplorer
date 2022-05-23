73c8cdb5f45e47aa2751a921c6535ff1685fd04f
Raj Singh <raj.singh@intersystems.com>
2022-05-23 14:58:52 -0400

    fix gunicorn location

--- flask_server_start.sh
+++ flask_server_start.sh
@@ -2,6 +2,6 @@
 
 cd ${FLASK_PATH}
 
-${PYTHON_PATH} /usr/irissys/bin/gunicorn --bind "0.0.0.0:8080" wsgi:app -w 4 2>&1
+${PYTHON_PATH} /home/irisowner/.local/bin/gunicorn --bind "0.0.0.0:8080" wsgi:app -w 4 2>&1
 
 exit 1
